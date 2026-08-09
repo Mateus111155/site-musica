93c45b22934385b17d7f75e361abc3a05bc2b8ad
Mateus111155 <mateusfelipe.batista@hotmail.com>
2026-08-09 10:11:32 -0300

    Add play and pause event listeners with alerts

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+const play = document.getElementById("play");
+const pause = document.getElementById("pause");
+
+play.addEventListener("click", function() {
+    alert("A música começou!");
+});
+
+pause.addEventListener("click", function() {
+    alert("A música foi pausada!");
+});
